754ac61f3cac958fb148bacb53a5ea8297b14666
MrYasuo <buiduchai_hsgs18@hus.edu.vn>
2022-04-18 20:45:42 +0700

    Merge branch 'main' of https://github.com/ZeroUetHackathon/ZeroTmpBackend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const fastify = require("fastify");
 const cookie = require("fastify-cookie");
 const cors = require("fastify-cors");
-const dotenv = require("dotenv");
-const path = require("path");
 
 const { db, config } = require("#configs");
 
@@ -10,7 +8,6 @@ const { users, auth } = require(`#routes`)[config.API_VERSION];
 
 const app = (opts = {}) => {
 	const app = fastify(opts);
-	dotenv.config({ path: path.join(__dirname, "../.env") });
 
 	/* -------------------- register cors ------------------- */
 	app.register(cors, {
@@ -25,8 +22,8 @@ const app = (opts = {}) => {
 	db();
 
 	/* -------------------- setup routes -------------------- */
-	app.register(users, { prefix: `/${config.API_VERSION}/users` });
-	app.register(auth, { prefix: `/${config.API_VERSION}/auth` });
+	app.register(users, { prefix: `/${config.API_VERSION}` });
+	app.register(auth, { prefix: `/${config.API_VERSION}` });
 
 	return app;
 };
