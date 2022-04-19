a206a10cfa843cfcf21179a5f5c970b37284eaa6
MrYasuo <buiduchai_hsgs18@hus.edu.vn>
2022-04-19 21:32:46 +0700

    add comment product controller

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -6,7 +6,9 @@ const status = require("http-status");
 module.exports = {
 	getProductsByProvince: async (request, reply) => {
 		const products = await Product.aggregate()
+			// match provinceId
 			.match({ provinceId: request.params.provinceId })
+			// lookup to get the wiki
 			.lookup({
 				from: "wikis",
 				localField: "wikiId",
@@ -17,9 +19,12 @@ module.exports = {
 			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
 		return reply.code(status.OK).send({ products });
 	},
+
 	getProductById: async (request, reply) => {
 		const product = await Product.aggregate()
+			// match _id field
 			.match({ _id: new mongoose.Types.ObjectId(request.params.productId) })
+			// lookup to get the wiki
 			.lookup({
 				from: "wikis",
 				localField: "wikiId",
@@ -30,11 +35,14 @@ module.exports = {
 			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
 		return reply.code(status.OK).send({ product: product[0] });
 	},
+
 	addProduct: async (request, reply) => {
 		const product = await Product.create(request.body);
 		return reply.code(status.OK).send({ product });
 	},
+
 	editProduct: async (request, reply) => {
+		// use {new: true} to return the updated product
 		const product = await Product.findByIdAndUpdate(
 			request.params.productId,
 			request.body,
@@ -44,10 +52,12 @@ module.exports = {
 			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
 		return reply.code(status.OK).send({ product });
 	},
+
 	deleteProduct: async (request, reply) => {
+		// return the deleted product
 		const product = await Product.findByIdAndDelete(request.params.productId);
 		if (!product)
 			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
-		return reply.code(status.OK).send({ product });
+		return reply.code(status.OK).send({ message: "Xoá sản phẩm thành công" });
 	},
 };
