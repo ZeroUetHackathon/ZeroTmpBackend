50a1cf7c66cf99b687f131b403b61cd86ac7aa09
MrYasuo <buiduchai_hsgs18@hus.edu.vn>
2022-04-20 23:30:50 +0700

    fix not found route handler

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -3,7 +3,7 @@ const { ApiError } = require("#utils");
 
 // eslint-disable-next-line
 const notFoundHandler = (request, reply) => {
-	throw new ApiError("Không tồn tại", status.NOT_FOUND);
+	throw new ApiError("Địa chỉ không tồn tại", status.NOT_FOUND);
 };
 
 module.exports = notFoundHandler;
