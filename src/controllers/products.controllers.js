842cfa7e786b99b086b3c426d067d211887bf3bc
MrYasuo <buiduchai_hsgs18@hus.edu.vn>
2022-04-23 05:05:50 +0700

    fix get product by province, restructure code

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -4,113 +4,127 @@ const { Product, Province, Sale } = require("#models");
 const { mongooseService } = require("#services");
 const { ApiError } = require("#utils");
 
-module.exports = {
-	getProductsByProvince: async (request, reply) => {
-		const products = await Province.aggregate([
-			// match provinceId
-			{
-				$match: {
-					_id: new mongoose.Types.ObjectId(request.params.provinceId),
-				},
+const getProductsByProvince = async (request, reply) => {
+	const products = await Province.aggregate([
+		// match provinceId
+		{
+			$match: {
+				code: request.params.provinceCode,
 			},
-			// lookup products
-			{
-				$lookup: {
-					from: "products",
-					localField: "_id",
-					foreignField: "provinceId",
-					pipeline: [
-						// lookup sales
-						{
-							$lookup: {
-								from: "sales",
-								localField: "_id",
-								foreignField: "productId",
-								as: "sale",
-							},
+		},
+		// lookup products
+		{
+			$lookup: {
+				from: "products",
+				localField: "_id",
+				foreignField: "provinceId",
+				pipeline: [
+					// lookup sales
+					{
+						$lookup: {
+							from: "sales",
+							localField: "_id",
+							foreignField: "productId",
+							as: "sale",
 						},
-						// get the total sold of all sales
-						{
-							$addFields: {
-								totalSold: {
-									$reduce: {
-										input: "$sale",
-										initialValue: 0,
-										in: {
-											$sum: ["$$this.sold", "$$value"],
-										},
+					},
+					// get the total sold of all sales
+					{
+						$addFields: {
+							totalSold: {
+								$reduce: {
+									input: "$sale",
+									initialValue: 0,
+									in: {
+										$sum: ["$$this.sold", "$$value"],
 									},
 								},
 							},
 						},
-						// sort by total sold
-						{ $sort: { totalSold: -1 } },
-					],
-					as: "products",
-				},
+					},
+					// sort by total sold
+					{ $sort: { totalSold: -1 } },
+				],
+				as: "products",
 			},
-			...mongooseService.addAttachmentsToWikiPlaceholder,
-		]);
-		if (products.length === 0)
-			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
-		return reply.code(status.OK).send({ products });
-	},
+		},
+		...mongooseService.addAttachmentsToWikiPlaceholder,
+	]);
+	if (products.length === 0)
+		throw new ApiError(status.NOT_FOUND, "Product not found");
+	return reply.code(status.OK).send({ products });
+};
 
-	getProductById: async (request, reply) => {
-		const product = await Product.aggregate([
-			// match _id field
-			{
-				$match: {
-					_id: new mongoose.Types.ObjectId(request.params.productId),
-				},
+const getProductById = async (request, reply) => {
+	const product = await Product.aggregate([
+		// match _id field
+		{
+			$match: {
+				_id: new mongoose.Types.ObjectId(request.params.productId),
 			},
-			{
-				$lookup: {
-					from: "sales",
-					localField: "_id",
-					foreignField: "productId",
-					as: "sales",
-				},
+		},
+		{
+			$lookup: {
+				from: "sales",
+				localField: "_id",
+				foreignField: "productId",
+				as: "sales",
 			},
-		]);
-		if (product.length === 0)
-			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
-		return reply.code(status.OK).send({ product: product[0] });
-	},
+		},
+	]);
+	if (product.length === 0)
+		throw new ApiError(status.NOT_FOUND, "Product not found");
+	return reply.code(status.OK).send({ product: product[0] });
+};
 
-	addProduct: async (request, reply) => {
-		const sales = request.body.sales.map((sale) =>
-			Object.assign(sale, { productId: request.params.productId })
-		);
-		const product = {
-			name: request.body.name,
-			category: request.body.category,
-			expire: request.body.expire,
-			provinceId: request.params.provinceId,
-		};
-		await Product.create(product);
-		await Sale.insertMany(sales);
-		return reply.code(status.OK).send({ message: "Tạo sản phẩm thành công" });
-	},
+const addProduct = async (request, reply) => {
+	const sales = request.body.sales.map((sale) =>
+		Object.assign(sale, { productId: request.body.productId })
+	);
+	const product = {
+		name: request.body.name,
+		category: request.body.category,
+		expire: request.body.expire,
+		provinceId: request.body.provinceId,
+	};
+	await Product.create(product);
+	await Sale.insertMany(sales);
+	return reply.code(status.OK).send({ message: "Success!" });
+};
 
-	editProduct: async (request, reply) => {
-		const product = await Product.findByIdAndUpdate(
-			request.params.productId,
-			request.body,
-			{ new: true }
-		);
-		if (!product)
-			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
-		return reply
-			.code(status.OK)
-			.send({ message: "Chỉnh sửa sản phẩm thành công" });
-	},
+const editProduct = async (request, reply) => {
+	const product = await Product.findByIdAndUpdate(
+		request.params.productId,
+		request.body,
+		{ new: true }
+	);
+	if (!product) throw new ApiError(status.NOT_FOUND, "Product not found");
+	return reply.code(status.OK).send({ message: "Success!" });
+};
 
-	deleteProduct: async (request, reply) => {
-		// return the deleted product
-		const product = await Product.findByIdAndDelete(request.params.productId);
-		if (!product)
-			throw new ApiError("Không tìm thấy sản phẩm nào", status.NOT_FOUND);
-		return reply.code(status.OK).send({ message: "Xoá sản phẩm thành công" });
-	},
+const deleteProduct = async (request, reply) => {
+	// return the deleted product
+	const product = await Product.findByIdAndDelete(request.params.productId);
+	if (!product) throw new ApiError(status.NOT_FOUND, "Product not found");
+	return reply.code(status.OK).send({ message: "Success!" });
+};
+
+const searchProduct = async (request, reply) => {
+	const { query } = request.body;
+	const products = await Product.aggregate([
+		{ $match: { $text: { $search: query } } },
+		{ $sort: { score: { $meta: "textScore" } } },
+		{ $project: { title: 1, _id: 0 } },
+	]);
+	if (!products) throw new ApiError(status.NOT_FOUND, "Product not found");
+	return reply.code(status.OK).send({ products });
+};
+
+module.exports = {
+	getProductById,
+	getProductsByProvince,
+	addProduct,
+	editProduct,
+	deleteProduct,
+	searchProduct,
 };
