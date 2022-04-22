7414787387a79263e72e3eb6d768cc637d8a9096
MrYasuo <buiduchai_hsgs18@hus.edu.vn>
2022-04-23 05:11:30 +0700

    fix cart route

--- src/routes/v1/carts.routes.js
+++ src/routes/v1/carts.routes.js
@@ -1,9 +1,9 @@
 const { cartsController } = require("#controllers");
 
 const carts = (fastify, opts, next) => {
-	fastify.get("/carts/:userId", cartsController.getAllCartsByUserId);
+	fastify.get("/:userId", cartsController.getAllCartsByUserId);
 	fastify.post("/cart", cartsController.addCart);
-	fastify.get("/cart/:cartId", cartsController.getCartById);
+	// fastify.get("/cart/:cartId", cartsController.getCartById);
 	fastify.put("/cart/:cartId", cartsController.editCart);
 	fastify.delete("/cart/:cartId", cartsController.deleteCart);
 	next();
